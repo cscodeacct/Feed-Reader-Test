c60f6f1ff47bece47fac16efe18a44caea1d4cac
Chigbo Okafor <40579127+cscodeacct@users.noreply.github.com>
2018-10-08 20:29:18 +0100

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,7 +26,7 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 //Credit: https://matthewcranford.com/feed-reader-walkthrough
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -38,7 +38,7 @@ $(function() {
 
         });
 
-        /* TODO: Write a test that loops through each feed
+        /*  Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -52,9 +52,9 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Write a new test suite named "The menu" */
     describe('A test suite named menu is defined', function () {
-        /* TODO: Write a test that ensures the menu element is
+        /* Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -62,7 +62,7 @@ $(function() {
         it('Menu element is hidden by default', function () {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
-         /* TODO: Write a test that ensures the menu changes
+         /* Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -76,9 +76,9 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Write a new test suite named "Initial Entries" */
     describe('A test suite named Initial Entries is defined', function () {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -87,31 +87,36 @@ $(function() {
         beforeEach(function (load) {
             loadFeed(0, load);
         });
-        it('loadFeed function is called and its work is completed', function (load) {
-            const entryElement = $('.feed .entry');
+        it('loadFeed function is called and its work is completed', function () {
+           const entryElement = $('.feed .entry');
             expect(entryElement.length).toBeGreaterThan(0);
-            load();
+            
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection is defined', function () {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        let oldFeed;
-        let newFeed;
+        loadFeed(0, function () {
+            oldFeed = $('.entry-link').attr('href');
+                loadFeed(1, function () {
+                    newFeed = $('.entry-link').attr('href');
+                        done();
+    });
+});
 
         beforeEach(function (load) {
             oldFeed = $('.entry-link').attr('href');
             loadFeed(1, load);
         });
 
-        it('loadFeed function content changes when a new Feed is loaded', function (load) {
+        it('loadFeed function content changes when a new Feed is loaded', function () {
             newFeed = $('.entry-link').attr('href');
             expect(newFeed).not.toBe(oldFeed);
-            load();
+            
         });
     });
 }());
